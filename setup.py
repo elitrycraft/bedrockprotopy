8d5883507940d3b63bd46aff8391c2f71d394ebe
elitrycraft <elitrycraft@gmail.com>
2025-07-16 20:07:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,14 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/elitrycraft/bedrockprotopy",
+    license="MIT",
     packages=find_packages(),
     install_requires=[
     ],
     classifiers=[
+        'Programming Language :: Python :: 3.11',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent'
     ],
     python_requires=">=3.7",
 )
