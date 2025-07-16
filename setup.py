cb83ed725907c473d32a3f26d189296ec6e6a810
elitrycraft <elitrycraft@gmail.com>
2025-07-16 18:40:54 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bedrockprotopy",
+    version="0.0.1",
+    author="Deniz1111212(elitrycraft)",
+    author_email="elitrycraft@outlook.com",
+    description="Minecraft bedrock edition protocol library written in python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/elitrycraft/bedrockprotopy",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+    ],
+    python_requires=">=3.7",
+)
