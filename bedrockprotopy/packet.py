4096918d8e7f94a610848a22cdb6eb535ae3e53d
Deniz1111212 <elitrycraft@gmail.com>
2026-02-22 13:45:40 +0300

    Update packet.py

--- bedrockprotopy/packet.py
+++ bedrockprotopy/packet.py
@@ -4,3 +4,15 @@ with open('packets.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 packets = data["packets"]
+
+def get_packet_by_id(packet_id):
+    for packet in packets:
+        if packet['id'] == packet_id:
+            return packet
+    return None
+
+def get_packet_by_name(packet_name):
+    for packet in packets:
+        if packet['name'] == packet_name:
+            return packet
+    return None
